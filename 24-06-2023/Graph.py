e530fb2a08d5f85cbbbdd7c34d60e57f7568f49a
sriramreddy-7 <sriramreddykoonadi@gmail.com>
2023-06-24 15:22:41 +0530

    GRAPH-BFS

--- 24-06-2023/Graph.py
+++ 24-06-2023/Graph.py
@@ -2,10 +2,7 @@ class Graph:
     def __init__(self,num_of_nodes,directed=True):
         self.m_num_of_nodes=num_of_nodes
         self.m_nodes=range(self.m_num_of_nodes)
-        
-        
         self.m_directed =directed
-        
         self.m_adj_list={node :set() for node in self.m_nodes}
         
     def add_edge(self,node1,node2,weight=1):
@@ -26,6 +23,3 @@ graph.add_edge(1,4,15)
 graph.add_edge(4,2,7)
 graph.add_edge(4,3,11)
 graph.print_adj_list()
-
-
-            
